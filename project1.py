1022930b0d33fe3957311d1bfeb1aeaaaf38245d
ronitcz-75 <ronnylohar20@gmail.com>
2026-02-27 13:20:30 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,12 @@
+user=["worker"," supervisor","user"," admin"]
+us=input("enter name: ")
+us in user
+for i in user:
+    if "admin" == us:
+        print("found it ")
+    else:
+       print("get out") 
+
+   
+
+    
